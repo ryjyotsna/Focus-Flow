8f05a2597caf48baf9e006e4da1d13c5b35d2cb7
Jyotsna Ray <ryjyotsna@gmail.com>
2026-02-10 23:57:41 +0530

    content added to scripts.js

--- script.js
+++ script.js
@@ -5,5 +5,21 @@ let workMinutes = 25;
 let breakMinutes = 5;
 let totalSeconds = workMinutes * 60;
 
-
+const startBtn = document.getElementById('startBtn');
+const resetBtn = document.getElementById('resetBtn');
+const settingsBtn = document.getElementById('settingsBtn');
+const minutesDisplay = document.getElementById('minutes');
+const secondsDisplay = document.getElementById('seconds');
+const currentTimeDisplay = document.getElementById('currentTime');
+const currentPeriodDisplay = document.getElementById('currentPeriod');
+const motivationDisplay = document.getElementById('motivation');
+const settingsModal = document.getElementById('settingsModal');
+const saveBtn = document.getElementById('saveBtn');
+const cancelBtn = document.getElementById('cancelBtn');
+const workTimeInput = document.getElementById('workTime');
+const breakTimeInput = document.getElementById('breakTime');
+const watcher = document.getElementById('watcher');
+const notificationSound = document.getElementById('notificationSound');
+const rainSound = document.getElementById('rainSound');
+const rainToggle = document.getElementById('rainToggle');
 
